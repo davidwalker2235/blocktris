37da9f8ebbb7c2c7286eefbdb0d87045f55c25a1
[deleted] <cada@erni-espana.es>
2019-08-18 13:12:01 +0200

    add credits images

---
+++ Assets/Scripts/BackButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButtonScript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Cover");
+    }
+}
---
+++ Assets/Scripts/InfoButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InfoButtonScript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
